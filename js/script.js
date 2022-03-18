8d66c990841d996e684bd04a90b5aa57f97f2656
Shannon Bulimo <shannonbulimo@gmail.com>
2022-03-18 13:32:48 +0300

    js intro

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+        $("#development").slideUp('1500');
+        $("#development-image").slideDown('1500');
+      });
+    });
