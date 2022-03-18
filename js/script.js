37a2641b4cea3b1c7fd24c110b565d62d51a8e35
Shannon Bulimo <shannonbulimo@gmail.com>
2022-03-18 13:41:16 +0300

    outlook changes

--- js/script.js
+++ js/script.js
@@ -13,3 +13,8 @@ $(document).ready(function(){
           $("#design-image").slideDown('1500').hide('1000');
           $("#design").show('1500');
         });
+        $("#design").click(function(){
+            $("#design").slideUp('1500');
+            $("#design-image").slideDown('1500');
+          });
+        });
