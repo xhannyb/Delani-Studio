4fd870fd0b4dde2ee58ac1f5ee88a4b88b13ad86
Shannon Bulimo <shannonbulimo@gmail.com>
2022-03-18 13:46:24 +0300

    js changes

--- js/script.js
+++ js/script.js
@@ -18,3 +18,21 @@ $(document).ready(function(){
             $("#design-image").slideDown('1500');
           });
         });
+        $(document).ready(function(){
+            $("#product-image").click(function(){
+              $("#product-image").slideDown('1500').hide('1000');
+              $("#product").show('1500');
+            });
+            $("#product").click(function(){
+              $("#product").slideUp('1500');
+              $("#product-image").slideDown('1500');
+            });
+          });
+          $(document).ready(function(){
+            $("#work1").mouseover(function(){
+              $("#overlay").show();
+            }).mouseout(function(){
+              $("#overlay").hide();
+            });
+          });
+          
